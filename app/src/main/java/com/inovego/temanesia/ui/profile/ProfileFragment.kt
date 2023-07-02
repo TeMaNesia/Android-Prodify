74f8968d5bdf2439a4a1584173eee9b3f1f7bb25
Hamza Firdaus <elhamzafirdaus@gmail.com>
2023-07-02 23:35:56 +0700

    Add sign out at profile page

--- app/src/main/java/com/inovego/temanesia/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/inovego/temanesia/ui/profile/ProfileFragment.kt
@@ -6,15 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.inovego.temanesia.R
 import com.inovego.temanesia.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
+    private lateinit var firebaseAuth: FirebaseAuth
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        firebaseAuth = FirebaseAuth.getInstance()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,12 +33,19 @@ class ProfileFragment : Fragment() {
         val profileViewModel = ViewModelProvider(this)[ProfileViewModel::class.java]
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
-
         profileViewModel.text.observe(viewLifecycleOwner) {
-            binding.actionBarCustom.tvUsername.text = it
+            binding.actionBarCustom.tvNamaLengkap.text = it
         }
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.actionBarCustom.icProfileSettingContainer.setOnClickListener {
+            firebaseAuth.signOut()
+            findNavController().navigate(R.id.action_navigation_profile_to_authActivity)
+            requireActivity().finish()
+        }
     }
 
     override fun onDestroyView() {
